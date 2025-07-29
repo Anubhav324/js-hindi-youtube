8b844e547a3e45c98343bce4b932994121c47452
Anubhav324 <abhi1rai2234@gmail.com>
2025-07-29 09:15:50 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+"use strict"; //treat all code as newer version
+//alert("hello") we are 7using node js not browser
+console.log(3+3)// code readability should be high
+let name="anubhav"
+let age=18
+let isLoggedin=false
+//number>=2 to power 53
+//bight
+//string=>""
+//boolean=>true/false
+//null=>standalone value
+//undefined=>
+//symbol
+console.log(typeof "anubhav");
+console.log(typeof undefined);//undefined
+console.log(typeof null);//null
+
