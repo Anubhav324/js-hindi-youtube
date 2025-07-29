87fcbd703aa3ef62d9d2ffd2a3860dffbee835ef
Anubhav324 <abhi1rai2234@gmail.com>
2025-07-29 09:16:15 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId=14423
+let accountEmail="abhi1223@gmail.com"
+var accountPassword="12345"
+accountCity="jaipur"
+accountEmail="sc.com"
+accountPassword="112332"
+let accountState;
+//accountId=2//
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountState])
+/*
+prefer not to use var*/
